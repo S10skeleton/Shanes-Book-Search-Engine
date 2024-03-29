3d8ad397c534cbf53f77c76afa49780864196bad
Shane <s10skeleton@gmail.com>
2024-03-29 02:02:47 -0600

    server.js file update

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -20,8 +20,13 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 };
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
+
 
 // create a new instance of Apollo Server using GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
